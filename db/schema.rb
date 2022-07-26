ff2402450b60916356c8f8e4f99717dd6436374d
jeena park <jeenapark15@gmail.com>
2022-07-26 11:15:31 -0400

    changed sqlite to pg

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_07_18_215627) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "entries", force: :cascade do |t|
     t.string "note"
     t.datetime "date"
