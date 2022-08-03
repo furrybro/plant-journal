368d77c152159a305a8ed5d2de8a3153416db356
jeena park <jeenapark15@gmail.com>
2022-08-03 11:04:55 -0400

    reverted partial login validation/error handling in login.jsx

--- app/javascript/packs/components/Login.jsx
+++ app/javascript/packs/components/Login.jsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from "react";
-import { Button, Form, FormGroup, Input, Label, FormText, FormFeedback } from "reactstrap";
+import React, { useState } from "react";
+import { Button, Form, FormGroup, Input, Label, FormText } from "reactstrap";
 import { useNavigate } from "react-router-dom";
 
 function Login({ setUser }) {
@@ -8,54 +8,22 @@ function Login({ setUser }) {
 
     const navigate = useNavigate();
 
-    const initialValues = {username:"", password: ""};
-    const [formValues, setFormValues] = useState(initialValues);
-    const [formErrors, setFormErrors] = useState({});
-    const [isSubmit, setIsSubmit] = useState(false);
-
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setFormValues({...formValues, [name]: value });
-        console.log(formValues);
-    };
-
-    const handleSubmit = (e) => {
+    function handleSubmit(e) {
         e.preventDefault();
-        setFormErrors(validate(formValues));
-        setIsSubmit(true);
+        fetch("/api/v1/login", {
+            method: "POST",
+            credentials: "include",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ username, password }),
+        }).then((r) => {
+            if (r.ok) {
+                r.json().then((user) => setUser(user));
+                navigate("/");
+            }
+        });
     }
-
-    useEffect(() => {
-        console.log(formErrors);
-        console.log(formValues, "formvalues")
-        if(Object.keys(formErrors).length === 0 && isSubmit) {
-            fetch("/api/v1/login", {
-                method: "POST",
-                credentials: "include",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify(formValues),
-            }).then((r) => {
-                if (r.ok) {
-                    r.json().then((user) => setUser(user));
-                    navigate("/");
-                }
-            });
-        } 
-    }, [formErrors])
-
-
-    const validate = (values) => {
-        const errors = {};
-        if (!values.username) {
-            errors.username = "Username is required!"
-        }
-        if (!values.password) {
-            errors.password = "Password is required!"
-        }
-        return errors;
-    };
 
     return (
         <div className="formdiv" style={{ fontFamily: 'Poppins' }}>
@@ -64,30 +32,22 @@ function Login({ setUser }) {
                 <FormGroup>
                     <Label htmlFor="username">Username:</Label>
                     <Input
-                        type="text"                        
-                        name="username" 
+                        type="text"
                         id="username"
                         autoComplete="off"
-                        value={formValues.username}
-                        onChange={handleChange}
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
                     />
-                    <p style={{ color: "red" }}>
-                        {formErrors.username}
-                    </p>
                 </FormGroup>
                 <FormGroup>
                     <Label htmlFor="password">Password:</Label>
                     <Input
                         type="password"
-                        name="password"
                         id="password"
                         autoComplete="current-password"
-                        value={formValues.password}
-                        onChange={handleChange}
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
                     />
-                     <p style={{ color: "red" }}>
-                        {formErrors.password}
-                    </p>
                 </FormGroup>
                 <Button type="submit">Login</Button>
                 <br></br>
