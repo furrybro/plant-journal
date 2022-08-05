a56aca72d358d0d0c0ba46687423a1eabb5ff4fa
jeena park <jeenapark15@gmail.com>
2022-08-05 12:04:20 -0400

    hide placeholders while organisms load

--- app/javascript/packs/components/Garden.jsx
+++ app/javascript/packs/components/Garden.jsx
@@ -3,7 +3,7 @@ import Organisms from "./Organisms";
 import OrganismForm from "./OrganismForm";
 
 function Garden({ user, setShowOrganismName }) {
-    const [organisms, setOrganisms] = useState([]);
+    const [organisms, setOrganisms] = useState(null);
     const [organismNameToEdit, setOrganismNameToEdit] = useState("");
     const [organismSpeciesToEdit, setOrganismSpeciesToEdit] = useState("");
     const [organismForm, setOrganismForm] = useState(false);
@@ -15,15 +15,18 @@ function Garden({ user, setShowOrganismName }) {
         fetch(`/api/v1/organisms/get_by_user/${user.id}`)
             .then(result => result.json())
             .then(result => setOrganisms(result));
-            setShowOrganismName("");
+        setShowOrganismName("");
     }, []);
 
+    if (organisms === null) {
+        return <div></div>
+    }
     return (
         <div>
             <div className="d-flex">
-                <Organisms organisms={organisms} setOrganismIdToEdit={setOrganismIdToEdit} setOrganismNameToEdit={setOrganismNameToEdit} setOrganismSpeciesToEdit={setOrganismSpeciesToEdit} setOrganismForm={setOrganismForm} modal={modal} setModal={setModal} deleteModal={deleteModal} setDeleteModal={setDeleteModal}/>
+                <Organisms organisms={organisms} setOrganismIdToEdit={setOrganismIdToEdit} setOrganismNameToEdit={setOrganismNameToEdit} setOrganismSpeciesToEdit={setOrganismSpeciesToEdit} setOrganismForm={setOrganismForm} modal={modal} setModal={setModal} deleteModal={deleteModal} setDeleteModal={setDeleteModal} />
             </div>
-            <OrganismForm user={user} setOrganisms={setOrganisms} organismIdToEdit={organismIdToEdit} organismNameToEdit={organismNameToEdit} setOrganismNameToEdit={setOrganismNameToEdit} organismSpeciesToEdit={organismSpeciesToEdit} setOrganismSpeciesToEdit={setOrganismSpeciesToEdit} organismForm={organismForm} setOrganismForm={setOrganismForm} modal={modal} setModal={setModal} deleteModal={deleteModal} setDeleteModal={setDeleteModal}/>
+            <OrganismForm user={user} setOrganisms={setOrganisms} organismIdToEdit={organismIdToEdit} organismNameToEdit={organismNameToEdit} setOrganismNameToEdit={setOrganismNameToEdit} organismSpeciesToEdit={organismSpeciesToEdit} setOrganismSpeciesToEdit={setOrganismSpeciesToEdit} organismForm={organismForm} setOrganismForm={setOrganismForm} modal={modal} setModal={setModal} deleteModal={deleteModal} setDeleteModal={setDeleteModal} />
         </div>
     );
 }
